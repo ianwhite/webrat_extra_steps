3e2e1ed34677e126cbd2e1a13f15b3f8f0d70c73
Ian White <ian.w.white@gmail.com>
2009-01-04 09:08:54 +1100

    Cleaned up Kosmas Schuetz's languages patch

--- rails_generators/extra_webrat_steps/extra_webrat_steps_generator.rb
+++ rails_generators/extra_webrat_steps/extra_webrat_steps_generator.rb
@@ -1,33 +1,21 @@
+require 'yaml'
+
 class ExtraWebratStepsGenerator < Rails::Generator::Base
-   require 'yaml'
-  
-  def initialize(*args)
-    
-    # Added support for the languages cucumber comes with
-    # 
-    # Load a file languages.yml containing the translations
-    # Get the language to use as arg
-    # If no language is given, the generator works as usual
-    
-    @langs = YAML.load_file(File.dirname(__FILE__) + '/templates/languages.yml')
-    @lang = args[0].to_s 
-    if @lang then
-      @features_path =  'features/' + @lang  
-    else
-      @features_path =  'features'
-      @lang = 'en'  
+  def initialize(args, options)
+    @features_path = 'features'
+    set_language(args.first)
+    unless File.exists?(webrat_steps = File.join(@features_path, 'step_definitions', 'webrat_steps.rb'))
+      raise "#{webrat_steps} not found, try running script/generate cucumber, or changing language"
     end
-    
-    File.exists?(@features_path + '/step_definitions/webrat_steps.rb') or raise "webrat_steps.rb not found, try running script/generate cucumber"
-    @original_webrat_steps = File.read(@features_path + '/step_definitions/webrat_steps.rb')
-    super(*args)
+    @original_webrat_steps = File.read(File.join(@features_path, "step_definitions/webrat_steps.rb"))
+    super(args, options)
   end
   
   def manifest
     record do |m|
-      m.directory File.join(@features_path + '/step_definitions')
-      m.template 'webrat_extra_steps.rb', File.join(@features_path + '/step_definitions', "webrat_extra_steps.rb"),
-        :assigns => {:scoped_methods => parse_scoped_methods}
+      m.directory File.join(@features_path, 'step_definitions')
+      m.template 'webrat_extra_steps.rb', File.join(@features_path, 'step_definitions', "webrat_extra_steps.rb"),
+        :assigns => {:scoped_methods => parse_scoped_methods, :features_path => @features_path, :language => @language}
     end
   end
   
@@ -35,9 +23,17 @@ protected
   def parse_scoped_methods
     methods = []
     @original_webrat_steps.scan(/\nWhen \/\^(.*?)\$\/ do \|(.*?)\|\n\s+(.+?)\s*\nend\n/m) do
-      words = @langs['en'].merge(@langs[@lang])
-      methods << "When /^#{words["within"]}: (.*?), #{$1}$/ do |scope, #{$2}|\n   within(scope) {|s| s.#{$3}}\nend"
+      methods << "When /^#{@translations["within"]}: (.*?), #{$1}$/ do |scope, #{$2}|\n   within(scope) {|s| s.#{$3}}\nend"
     end
     methods
+  end
+  
+  # Load up the transations for the passed language, and modify the features_path accordingly
+  def set_language(language)
+    @language = language
+    languages = YAML.load_file(File.join(File.dirname(__FILE__), 'templates/languages.yml'))
+    @features_path = File.join(@features_path, @language) if @language
+    @translations = languages['en']
+    @translations.merge!(languages[@language]) if @language
   end
 end
