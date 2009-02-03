65dd6ef6ff0ae762d337da5467942223d110ff61
Ian White <ian.w.white@gmail.com>
2009-02-03 18:59:54 +0000

    Created scoped variants of 'Then I should see...' steps

--- rails_generators/extra_webrat_steps/extra_webrat_steps_generator.rb
+++ rails_generators/extra_webrat_steps/extra_webrat_steps_generator.rb
@@ -16,9 +16,22 @@ class ExtraWebratStepsGenerator < Rails::Generator::Base
 protected
   def parse_scoped_methods
     methods = []
+    
+    # remove 'When I go to' as that doesn't make sense to be scoped
+    @original_webrat_steps.sub(/\nWhen \/\^I go to.*?\nend\n/,'')
+    
+    # create scoped method variants of all of the 'When' steps
     @original_webrat_steps.scan(/\nWhen \/\^(.*?)\$\/ do \|(.*?)\|\n\s+(.+?)\s*\nend\n/m) do
-      methods << "When /^within: (.*?), #{$1}$/ do |scope, #{$2}|\n   within(scope) {|s| s.#{$3}}\nend"
+      methods << "When /^within: (.*?), #{$1}$/ do |scope, #{$2}|\n  within(scope) {|s| s.#{$3}}\nend"
     end
+    
+    # add scoped versions of 'I should see' steps
+    methods << "Then /^within: (.*?), I should see \"(.*)\"$/ do |scope, text|\n" +
+               "  within(scope) {|s| s.dom.should contain(text)}\n" +
+               "end"
+    methods << "Then /^within: (.*?), I should not see \"(.*)\"$/ do |scope, text|\n" +
+               "  within(scope) {|s| s.dom.should_not contain(text)}\n" +
+               "end"
     methods
   end
 end
